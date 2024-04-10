43b582d55fbf3e24fcae5c35f24c1ab21690df62
Tom Gutierrez <pro.tom.gutierrez@gmail.com>
2024-04-10 10:13:31 +0200

    :art:
    Dépôt de l'arborescence de GestionFichier
    
    - iut.info1.huffman.fichier
    
    Co-Authored-By: Romain1427 <150252971+Romain1427@users.noreply.github.com>

---
+++ iut/info1/huffman/fichier/GestionFichier.java
@@ -0,0 +1,19 @@
+/**
+ * GestionFichier.java          09/04/2024
+ * IUT DE RODEZ            Pas de copyrights
+ */
+package iut.info1.huffman.fichier;
+
+
+/**
+ * TODO commenter cette classe
+ */
+public class GestionFichier {
+
+    /**
+     * TODO commenter la méthode
+     */
+    public static void affichageFichier(String[] liste){
+        System.out.println("stub");
+    }
+}
