f810c500370b7c93836e1d41eddaf53f1e36c31e
Tom Gutierrez <pro.tom.gutierrez@gmail.com>
2024-04-10 11:38:59 +0200

    :sparkles:
    Ajout de la méthode affichageFichier.
    
    Ajout des tests :
    - Ajout du package iut.info1.test.huffman.fichier
    - Ajout de la classe TestGestionFichier et de la méthode de test : testAffichageFichier
    
    Co-Authored-By: Romain1427 <150252971+Romain1427@users.noreply.github.com>

--- iut/info1/huffman/fichier/GestionFichier.java
+++ iut/info1/huffman/fichier/GestionFichier.java
@@ -11,9 +11,18 @@ package iut.info1.huffman.fichier;
 public class GestionFichier {
 
     /**
-     * TODO commenter la méthode
+     * L'objectif est d'afficher le contenu d'un tableau de
+     * chaîne de caractères, pour qui chaque éléments de
+     * cette chaîne est la ligne d'un fichier.
+     * \n
+     * Dans le cas où il existe plusieurs saut à la ligne vide, ces
+     * derniers seront conservés.
+     * 
+     * @param contenuFichier Tableau de chaîne de caractère
      */
-    public static void affichageFichier(String[] liste){
-        System.out.println("stub");
+    public static void affichageFichier(String[] contenuFichier){
+        for (String ligne : contenuFichier) {
+            System.out.println(ligne);
+        }
     }
 }
---
+++ iut/info1/test/huffman/TestGestionFichier.java
@@ -0,0 +1,73 @@
+package iut.info1.test.huffman.fichier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import iut.info1.huffman.fichier.GestionFichier;
+
+class TestGestionFichier {
+    
+    private final PrintStream sortieStandard = System.out;
+    private final ByteArrayOutputStream capteurDeSortie = new ByteArrayOutputStream();
+    
+    
+    /** TODO Faire la javadoc de CONTENU_DE_FICHIERS*/
+    public static final String[][] CONTENU_DE_FICHIERS = {
+        {"ABCD"},
+        {"Lorem", "", "Ipsum"},
+        {"a", "", "", "",  "b"},
+        {"", "", "",  ""},
+        {"Lorem", "DELETE FROM mysql.user", "Ipsum",  "Dolor sit amet"},
+        {}
+    };
+    
+    /** TODO Faire la javadoc de RESULTAT_POUR_CONTENU*/
+    public static final String[] RESULTAT_POUR_CONTENU = {
+            
+            "ABCD\r\n",
+            "Lorem\r\n\r\nIpsum\r\n",
+            "a\r\n\r\n\r\n\r\nb\r\n",
+            "\r\n\r\n\r\n\r\n",
+            "Lorem\r\nDELETE FROM mysql.user\r\nIpsum\r\nDolor sit amet\r\n",
+            ""
+        };
+    
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println("------TEST DE LA CLASSE GestionFichier------");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println("---FIN DE TEST DE LA CLASSE GestionFichier---");
+
+    }
+
+    @Test
+    void testAffichageFichier() {
+
+        for (int indiceTest = 0;
+                indiceTest < CONTENU_DE_FICHIERS.length;
+                indiceTest++) {
+
+            System.setOut(new PrintStream(capteurDeSortie));
+            
+            GestionFichier.affichageFichier(CONTENU_DE_FICHIERS[indiceTest]);                
+
+            assertEquals(RESULTAT_POUR_CONTENU[indiceTest],
+                        capteurDeSortie.toString());
+
+            System.setOut(sortieStandard);
+            
+            capteurDeSortie.reset();
+        }
+        
+    }
+
+}
