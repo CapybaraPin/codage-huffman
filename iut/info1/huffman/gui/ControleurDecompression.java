2e02b18b81cffb7b8ecbe2c97ca23cb29f802581
Leminithierry <166013407+thierry-erwan@users.noreply.github.com>
2024-06-03 14:03:06 +0200

    Rajout du pop up de décompression, affichage de l'image correct

--- iut/info1/huffman/gui/ControleurDecompression.java
+++ iut/info1/huffman/gui/ControleurDecompression.java
@@ -1,3 +1,4 @@
+
 /**
  * ConroleurDecompression.java		28/05/2024
  * IUT DE RODEZ						Pas de copyrights
@@ -5,17 +6,15 @@
 package iut.info1.huffman.gui;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.Optional;
 
 import iut.info1.huffman.Decompression;
-import iut.info1.huffman.arbre.ArbreBinaireHuffman;
-import iut.info1.huffman.fichier.GestionFichier;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
@@ -25,142 +24,147 @@ import javafx.stage.FileChooser;
  */
 public class ControleurDecompression {
 
-	private String cheminFichierBinChoisi;
+    private String cheminFichierBinChoisi;
 
-	private String cheminFichierCleChoisi;
-	
-	private String cheminDossierDecompressionChoisi;
-	
-	@FXML
-	private Label cheminFichierBin;
-	
-	@FXML 
-	private Label nomFichierBin;
-	
-	@FXML 
-	private Label tailleFichierBin;
-	
-	@FXML 
-	private Label cheminFichierCle;
-	
-	@FXML 
-	private Label cheminDossierResultat;
-	
-	@FXML
-	private Button boutonDecompression;
-	
+    private String cheminFichierCleChoisi;
+
+    private String cheminDossierDecompressionChoisi;
+
+    @FXML
+    private Label cheminFichierBin;
+
+    @FXML 
+    private Label nomFichierBin;
+
+    @FXML 
+    private Label tailleFichierBin;
+
+    @FXML 
+    private Label cheminFichierCle;
+
+    @FXML 
+    private Label cheminDossierResultat;
+
+    @FXML
+    private Button boutonDecompression;
+
     @FXML
     private Button retour;
-	
-	@FXML
-	private void initialize() {
 
-		cheminFichierBinChoisi = null;
-		cheminFichierCleChoisi = null;
-		cheminDossierDecompressionChoisi = null;
-		
-	}
-	
-	@FXML
-	private void saisirCheminBin() {
-		
-		FileChooser choixFichier = new FileChooser();
-		
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("BIN files (*.bin)", "*.bin");
-		choixFichier.getExtensionFilters().add(extFilter);
-		
-		File fichierChoisi = choixFichier.showOpenDialog(null);
-		
-		
-		cheminFichierBinChoisi = null;
-		if (fichierChoisi != null) {
-			cheminFichierBinChoisi = fichierChoisi.getAbsolutePath();
-			cheminFichierBin.setText(cheminFichierBinChoisi);
-			nomFichierBin.setText(fichierChoisi.getName());
-			tailleFichierBin.setText(String.valueOf(fichierChoisi.length()) + " octet(s)");
-			
-		} else {
-			cheminFichierBin.setText("...");
-			nomFichierBin.setText("...");
-			tailleFichierBin.setText("...");
-		}
-		
-		actualisationValiditeDecompression();
-	}
-	
-	@FXML
-	private void saisirCheminFichierCle() {
-		
-		FileChooser choixFichier = new FileChooser();
-		
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("fichiers encode (_EncodeH.txt)", "*_EncodeH.txt");
-		choixFichier.getExtensionFilters().add(extFilter);
-		
-		File fichierChoisi = choixFichier.showOpenDialog(null); 
-		
-		cheminFichierCleChoisi = null;
-		if (fichierChoisi != null) {
-			cheminFichierCleChoisi = fichierChoisi.getAbsolutePath();
-			cheminFichierCle.setText(cheminFichierCleChoisi);
-			
-		} else {
-			cheminFichierCle.setText("...");
+    @FXML
+    private void initialize() {
 
-		}
-		
-		actualisationValiditeDecompression();
-	}
-	
-	@FXML
-	private void saisirCheminDecompression() {
-		
-		DirectoryChooser parcoursDossier = new DirectoryChooser();
-		
-		File dossierChoisi = parcoursDossier.showDialog(null);
-		
-		cheminDossierDecompressionChoisi = null;
-		if (dossierChoisi != null) {
+	cheminFichierBinChoisi = null;
+	cheminFichierCleChoisi = null;
+	cheminDossierDecompressionChoisi = null;
 
-			cheminDossierDecompressionChoisi = dossierChoisi.getPath() + "\\";
+    }
 
-			cheminDossierResultat.setText(dossierChoisi.getPath() + "\\");
-		} else {
-			cheminDossierResultat.setText("...");
-	
-		}
-		
-		actualisationValiditeDecompression();
+    @FXML
+    private void saisirCheminBin() {
+
+	FileChooser choixFichier = new FileChooser();
+
+	FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("BIN files (*.bin)", "*.bin");
+	choixFichier.getExtensionFilters().add(extFilter);
+
+	File fichierChoisi = choixFichier.showOpenDialog(null);
+
+
+	cheminFichierBinChoisi = null;
+	if (fichierChoisi != null) {
+	    cheminFichierBinChoisi = fichierChoisi.getAbsolutePath();
+	    cheminFichierBin.setText(cheminFichierBinChoisi);
+	    nomFichierBin.setText(fichierChoisi.getName());
+	    tailleFichierBin.setText(String.valueOf(fichierChoisi.length()) + " octet(s)");
+
+	} else {
+	    cheminFichierBin.setText("...");
+	    nomFichierBin.setText("...");
+	    tailleFichierBin.setText("...");
 	}
-	
-	@FXML
-	private void lancerDecompression() {
-			
-		
-		Decompression decompression;
-		
-		decompression = new Decompression(cheminFichierBinChoisi, cheminFichierCleChoisi, cheminDossierDecompressionChoisi);
-		decompression.execute();
-   
+
+	actualisationValiditeDecompression();
+    }
+
+    @FXML
+    private void saisirCheminFichierCle() {
+
+	FileChooser choixFichier = new FileChooser();
+
+	FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("fichiers encode (_EncodeH.txt)", "*_EncodeH.txt");
+	choixFichier.getExtensionFilters().add(extFilter);
+
+	File fichierChoisi = choixFichier.showOpenDialog(null); 
+
+	cheminFichierCleChoisi = null;
+	if (fichierChoisi != null) {
+	    cheminFichierCleChoisi = fichierChoisi.getAbsolutePath();
+	    cheminFichierCle.setText(cheminFichierCleChoisi);
+
+	} else {
+	    cheminFichierCle.setText("...");
+
 	}
-		
-	/**
-	 * Permet de réactiver ou de désactiver le bouton de décompression
-	 * si les paramètres mis en place sont vérifiées
-	 */
-	private void actualisationValiditeDecompression() {
-		
-		boutonDecompression.setDisable(true);
-		
-		if (   cheminFichierBinChoisi != null 
-			&& cheminDossierDecompressionChoisi != null
-			&& cheminFichierCleChoisi != null) {
-			
-			boutonDecompression.setDisable(false);
-		}
+
+	actualisationValiditeDecompression();
+    }
+
+    @FXML
+    private void saisirCheminDecompression() {
+
+	DirectoryChooser parcoursDossier = new DirectoryChooser();
+
+	File dossierChoisi = parcoursDossier.showDialog(null);
+
+	cheminDossierDecompressionChoisi = null;
+	if (dossierChoisi != null) {
+
+	    cheminDossierDecompressionChoisi = dossierChoisi.getPath() + "\\";
+
+	    cheminDossierResultat.setText(dossierChoisi.getPath() + "\\");
+	} else {
+	    cheminDossierResultat.setText("...");
+
 	}
-	
-	@FXML
-	void annuler(ActionEvent event) {
-		Main.activerMenu();
+
+	actualisationValiditeDecompression();
+    }
+
+    @FXML
+    private void lancerDecompression() {
+	Decompression decompression;
+
+	Alert boiteAlerte = new Alert(Alert.AlertType.CONFIRMATION,
+		"Êtes-vous sûr de vouloir décompresser ce fichier ?",
+		ButtonType.YES, ButtonType.NO);
+
+	Optional<ButtonType> option = boiteAlerte.showAndWait();
+	if (option.get() == ButtonType.YES) {
+
+	    decompression = new Decompression(cheminFichierBinChoisi, cheminFichierCleChoisi, cheminDossierDecompressionChoisi);
+	    decompression.execute();
+	} // else
+    }
+
+    /**
+     * Permet de réactiver ou de désactiver le bouton de décompression
+     * si les paramètres mis en place sont vérifiées
+     */
+    private void actualisationValiditeDecompression() {
+
+	boutonDecompression.setDisable(true);
+
+	if (   cheminFichierBinChoisi != null 
+		&& cheminDossierDecompressionChoisi != null
+		&& cheminFichierCleChoisi != null) {
+
+	    boutonDecompression.setDisable(false);
 	}
+    }
+
+    @FXML
+    void annuler(ActionEvent event) {
+	Main.activerMenu();
+    }
 }
--- iut/info1/huffman/gui/Main.java
+++ iut/info1/huffman/gui/Main.java
@@ -3,65 +3,64 @@ package iut.info1.huffman.gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-// import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 public class Main extends Application {
 
-	private static Scene menuApplicationHuffman;
+    private static Scene menuApplicationHuffman;
 
-	private static Scene menuCompressionHuffman;
+    private static Scene menuCompressionHuffman;
 
-	private static Scene menuDecompressionHuffman;
+    private static Scene menuDecompressionHuffman;
 
-	private static Stage fenetrePrincipale; 
+    private static Stage fenetrePrincipale; 
 
-	public static void activerCompression() {
-		fenetrePrincipale.setScene(menuCompressionHuffman);
+    public static void activerCompression() {
+	fenetrePrincipale.setScene(menuCompressionHuffman);
+    }
+
+    public static void activerDecompression() {
+	fenetrePrincipale.setScene(menuDecompressionHuffman);
+    }
+
+    public static void activerMenu() {
+	fenetrePrincipale.setScene(menuApplicationHuffman);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+	try {
+	    /* Chargement de la page principale */
+	    FXMLLoader chargeurFXML = new FXMLLoader();
+	    chargeurFXML.setLocation(getClass().getResource("menuApplicationHuffman.fxml"));
+	    Parent conteneur = chargeurFXML.load();
+	    menuApplicationHuffman = new Scene(conteneur,625,240);
+
+	    /* Chargement de la page de compression */
+	    FXMLLoader chargeurFXMLCompression = new FXMLLoader();
+	    chargeurFXMLCompression.setLocation(getClass().getResource("menuCompressionHuffman.fxml"));
+	    conteneur = chargeurFXMLCompression.load();
+	    menuCompressionHuffman = new Scene(conteneur,550,350);
+
+	    /* Chargement de la page de dÃ©compression */
+	    FXMLLoader chargeurFXMLDecompression = new FXMLLoader();
+	    chargeurFXMLDecompression.setLocation(getClass().getResource("menuDecompressionHuffman.fxml"));
+	    conteneur = chargeurFXMLDecompression.load();
+	    menuDecompressionHuffman = new Scene(conteneur,600,400);
+
+	    /* Ce qui concerne la page principale */
+	    primaryStage.setTitle("Application Huffman-V0.3.2-alpha");
+	    primaryStage.setScene(menuApplicationHuffman);
+	    fenetrePrincipale = primaryStage;
+	    primaryStage.show();
+	} catch(Exception e) {
+	    e.printStackTrace();
 	}
+    }
 
-	public static void activerDecompression() {
-		fenetrePrincipale.setScene(menuDecompressionHuffman);
-	}
-
-	public static void activerMenu() {
-		fenetrePrincipale.setScene(menuApplicationHuffman);
-	}
-
-	@Override
-	public void start(Stage primaryStage) {
-
-		try {
-			/* Chargement de la page principale */
-			FXMLLoader chargeurFXML = new FXMLLoader();
-			chargeurFXML.setLocation(getClass().getResource("menuApplicationHuffman.fxml"));
-			Parent conteneur = chargeurFXML.load();
-			menuApplicationHuffman = new Scene(conteneur,625,240);
-
-			/* Chargement de la page de compression */
-			FXMLLoader chargeurFXMLCompression = new FXMLLoader();
-			chargeurFXMLCompression.setLocation(getClass().getResource("menuCompressionHuffman.fxml"));
-			conteneur = chargeurFXMLCompression.load();
-			menuCompressionHuffman = new Scene(conteneur,550,350);
-
-			/* Chargement de la page de dÃ©compression */
-			FXMLLoader chargeurFXMLDecompression = new FXMLLoader();
-			chargeurFXMLDecompression.setLocation(getClass().getResource("menuDecompressionHuffman.fxml"));
-			conteneur = chargeurFXMLDecompression.load();
-			menuDecompressionHuffman = new Scene(conteneur,600,400);
-
-			/* Ce qui concerne la page principale */
-			primaryStage.setTitle("Application Huffman-V0.2.1-alpha");
-			primaryStage.setScene(menuApplicationHuffman);
-			fenetrePrincipale = primaryStage;
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+	launch(args);
+    }
 }
